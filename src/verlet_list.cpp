b8d396cc93cf18e72cc8a931009e450b7b5e7f5b
Denis Taniguchi <denis.taniguchi@ncl.ac.uk>
2018-07-31 23:45:02 +0100

    Solving issues with grid having to be extended by the cutoff distance.

--- src/verlet_list.cpp
+++ src/verlet_list.cpp
@@ -24,10 +24,9 @@ struct input_params
   double upper_y;
   double lower_z;
   double upper_z;
-  int cell_count_x;
-  int cell_count_y;
-  int cell_count_z;
-
+  double cell_size;
+  std::size_t step_count;
+  
   void load(const std::string &filename)
   {
     namespace pt = boost::property_tree;
@@ -42,9 +41,8 @@ struct input_params
     upper_x = tree.get<double>("input.upper.x");
     upper_y = tree.get<double>("input.upper.y");
     upper_z = tree.get<double>("input.upper.z");
-    cell_count_x = tree.get<int>("input.cell_count.x");
-    cell_count_y = tree.get<int>("input.cell_count.y");
-    cell_count_z = tree.get<int>("input.cell_count.z");
+    cell_size = tree.get<double>("input.cell_size");
+    step_count = tree.get<std::size_t>("input.step_count");
   }
 
   void save(const std::string &filename)
@@ -60,9 +58,8 @@ struct input_params
     tree.put("input.upper.x", upper_x);
     tree.put("input.upper.y", upper_y);
     tree.put("input.upper.z", upper_z);
-    tree.put("input.cell_count.x", cell_count_x);
-    tree.put("input.cell_count.y", cell_count_y);
-    tree.put("input.cell_count.z", cell_count_z);
+    tree.put("input.cell_size", cell_size);
+    tree.put("input.step_count", step_count);
     pt::write_xml(filename, tree);
   }
 };
@@ -75,20 +72,17 @@ void random_fill(ForwardIterator begin, ForwardIterator end, double lower, doubl
   std::generate(begin, end, [&]() { return dist(rng); });
 }
 
-double cell_distance(int i, int j, int k
-		     , double cell_size_x
-		     , double cell_size_y
-		     , double cell_size_z)
+double cell_distance(int i, int j, int k, double cell_size)
 {
   double x = 0;
-  int ai = std::abs(i);
-  if (ai > 0) x = (ai - 1) * cell_size_x;
+  if (i > 0) x = (i - 1) * cell_size;
+  else if (i < 0) x = (i + 1) * cell_size;
   double y = 0;
-  int aj = std::abs(j);
-  if (aj > 0) y = (aj - 1) * cell_size_y;
+  if (j > 0) y = (j - 1) * cell_size;
+  else if (j < 0) y = (j + 1) * cell_size;
   double z = 0;
-  int ak = std::abs(k);
-  if (ak > 0) z = (ak - 1) * cell_size_z;
+  if (k > 0) z = (k - 1) * cell_size;
+  else if (k < 0) z = (k - 1) * cell_size;
   return std::sqrt(x * x + y * y + z * z);
 }
 
@@ -127,28 +121,28 @@ int main(int argc, char **argv)
   
   size_t particle_count = ip.particle_count;
   double neighbor_cutoff = ip.neighbor_cutoff;
-  const double lower_x = ip.lower_x;
-  const double upper_x = ip.upper_x;
-  const double lower_y = ip.lower_y;
-  const double upper_y = ip.upper_y;
-  const double lower_z = ip.lower_z;
-  const double upper_z = ip.upper_z;
-  const int cell_count_x = ip.cell_count_x;
-  const int cell_count_y = ip.cell_count_y;
-  const int cell_count_z = ip.cell_count_z;
+  const double lower_x = ip.lower_x - neighbor_cutoff;
+  const double lower_y = ip.lower_y - neighbor_cutoff;
+  const double lower_z = ip.lower_z - neighbor_cutoff;
+  const double upper_x = ip.upper_x + neighbor_cutoff;
+  const double upper_y = ip.upper_y + neighbor_cutoff;
+  const double upper_z = ip.upper_z + neighbor_cutoff;
+  const double cell_size = ip.cell_size;
+  const int cell_count_x = std::ceil((upper_x - lower_x) / cell_size);
+  const int cell_count_y = std::ceil((upper_y - lower_y) / cell_size);
+  const int cell_count_z = std::ceil((upper_z - lower_z) / cell_size);
   const int max_cell_count = cell_count_x * cell_count_y * cell_count_z;
-  const double cell_size_x = (upper_x - lower_x) / cell_count_x;
-  const double cell_size_y = (upper_y - lower_y) / cell_count_y;
-  const double cell_size_z = (upper_z - lower_z) / cell_count_z;
+
+  const std::size_t step_count = ip.step_count;
   
   std::vector<double> x(particle_count);
-  random_fill(x.begin(), x.end(), lower_x, upper_x);
+  random_fill(x.begin(), x.end(), ip.lower_x, ip.upper_x);
 
   std::vector<double> y(particle_count);
-  random_fill(y.begin(), y.end(), lower_y, upper_y);
+  random_fill(y.begin(), y.end(), ip.lower_y, ip.upper_y);
   
   std::vector<double> z(particle_count);
-  random_fill(z.begin(), z.end(), lower_z, upper_z);
+  random_fill(z.begin(), z.end(), ip.lower_z, ip.upper_z);
 
   typedef Kokkos::View<
     double *
@@ -184,46 +178,14 @@ int main(int argc, char **argv)
   Kokkos::View<int *> d_cell_indices("cell_indices", particle_count);
   Kokkos::View<int *> d_particle_per_cell("particle_per_cell", cell_count);
   Kokkos::View<int **> d_cell_particles("cell_particles", cell_count, max_particle_per_cell);  
-
-  Kokkos::View<int> d_resize("resize");
-  auto h_resize = Kokkos::create_mirror_view(d_resize);
-  h_resize() = 1;
-  while(h_resize())
-  {
-    h_resize() = 0;
-    deep_copy(d_resize, h_resize);
-    Kokkos::Profiling::pushRegion("cell list");
-    Kokkos::parallel_for("cell list", particle_count
-      , KOKKOS_LAMBDA(const int i)
-	{
-	  int x = (d_x(i) - lower_x) / cell_size_x;
-	  int y = (d_y(i) - lower_y) / cell_size_y;
-	  int z = (d_z(i) - lower_z) / cell_size_z;
-	  int cell = x + y * cell_count_x + z * cell_count_x * cell_count_y;
-	  d_cell_indices(i) = cell;
-	  int j = Kokkos::atomic_fetch_add(&d_particle_per_cell(cell), 1);
-	  if (j < d_cell_particles.extent(1))
-	    d_cell_particles(cell, j) = i;
-	  else
-	    d_resize() = 1;
-	});
-    Kokkos::Profiling::popRegion();
-    deep_copy(h_resize, d_resize);
-    if(h_resize())
-    {
-      max_particle_per_cell *= 2;
-      d_cell_particles = Kokkos::View<int **>("cell_particles", cell_count, max_particle_per_cell);
-      Kokkos::deep_copy(d_particle_per_cell, 0);
-    }
-  }
-
-  int stencil_count_x = std::ceil(neighbor_cutoff / cell_size_x);
-  int stencil_count_y = std::ceil(neighbor_cutoff / cell_size_y);
-  int stencil_count_z = std::ceil(neighbor_cutoff / cell_size_z);
+  
+  int stencil_count_x = std::ceil(neighbor_cutoff / cell_size);
+  int stencil_count_y = std::ceil(neighbor_cutoff / cell_size);
+  int stencil_count_z = std::ceil(neighbor_cutoff / cell_size);
   std::size_t max_stencil_count = (2 * stencil_count_x + 1)
     * (2 * stencil_count_y + 1)
     * (2 * stencil_count_z + 1);
-  std::cout << "max_stencil_count: " << max_stencil_count << std::endl;
+
   Kokkos::View<int *> d_stencil = Kokkos::View<int *>("stencil", max_stencil_count);
   auto h_stencil = Kokkos::create_mirror_view(d_stencil);
   int stencil_count = 0;
@@ -231,102 +193,163 @@ int main(int argc, char **argv)
   {
     for (int j = -stencil_count_y; j <= stencil_count_y; j++)
     {
-      for (int i = -stencil_count_z; i <= stencil_count_z; i++)
+      for (int i = -stencil_count_x; i <= stencil_count_x; i++)
       {
-	if (cell_distance(i, j, k, cell_size_x, cell_size_y, cell_size_z) < neighbor_cutoff)
-	  h_stencil(stencil_count++) = i + j * cell_count_x + k * cell_count_x * cell_count_y;
+        if (cell_distance(i, j, k, cell_size) < neighbor_cutoff)
+          h_stencil(stencil_count++) = i + j * cell_count_x + k * cell_count_x * cell_count_y;
       }
     }
   }
-  for (int i = 0; i < stencil_count; i++)
-    std::cout << i << ": " << h_stencil(i) << " ";
-  std::cout << std::endl;
   Kokkos::deep_copy(d_stencil, h_stencil);
-
+  
   std::size_t max_neighbor_count = 16;
   Kokkos::View<int *> d_neighbor_count = Kokkos::View<int *>("neighbor_count", particle_count);
   Kokkos::View<int **> d_neighbors = Kokkos::View<int **>("neighbors", particle_count, max_neighbor_count);
 
-  h_resize() = 1;
-  while(h_resize())
+  Kokkos::View<int> d_resize("resize");
+  auto h_resize = Kokkos::create_mirror_view(d_resize);
+  for (int step = 0; step < step_count; step++)
   {
-    h_resize() = 0;
-    Kokkos::deep_copy(d_resize, h_resize);
-    Kokkos::Profiling::pushRegion("neighbor list");
-    Kokkos::parallel_for("neighbor list", particle_count, KOKKOS_LAMBDA(const int i)
-      {
-	double x = d_x(i);
-	double y = d_y(i);
-	double z = d_z(i);
-	int cell = d_cell_indices(i);
-	for (int s = 0; s < stencil_count; s++)
-	{
-	  cell += d_stencil(s);
-	  if (cell >= 0 && cell < max_cell_count)
-	  {
-	    for (int p = 0; p < d_particle_per_cell(cell); p++)
-	    {
-	      int j = d_cell_particles(cell, p);
-	      double dx = d_x(j) - x;
-	      double dy = d_y(j) - y;
-	      double dz = d_z(j) - z;
-	      double d = sqrt(dx * dx + dy * dy + dz * dz);
-	      if (d < neighbor_cutoff && i != j)
-	      {
-		int n = Kokkos::atomic_fetch_add(&d_neighbor_count(i), 1);
-		if (n < max_neighbor_count)
-		  d_neighbors(i, n) = j;
-		else
-		  d_resize() = 1;
-	      }
-	    }
-	  }
-	}
-      });
-    Kokkos::Profiling::popRegion();
-    Kokkos::deep_copy(h_resize, d_resize);
-    if (h_resize())
+    h_resize() = 1;
+    while(h_resize())
     {
-      max_neighbor_count *= 2;
-      d_neighbors = Kokkos::View<int **>("neighbors", particle_count, max_neighbor_count);
-      Kokkos::deep_copy(d_neighbor_count, 0);
+      h_resize() = 0;
+      deep_copy(d_resize, h_resize);
+      Kokkos::deep_copy(d_particle_per_cell, 0);
+      Kokkos::Profiling::pushRegion("cell list");
+      Kokkos::parallel_for("cell list", particle_count
+			   , KOKKOS_LAMBDA(const int i)
+                           {
+                             int x = (d_x(i) - lower_x) / cell_size;
+                             int y = (d_y(i) - lower_y) / cell_size;
+                             int z = (d_z(i) - lower_z) / cell_size;
+                             int cell = x + y * cell_count_x + z * cell_count_x * cell_count_y;
+                             d_cell_indices(i) = cell;
+                             int j = Kokkos::atomic_fetch_add(&d_particle_per_cell(cell), 1);
+                             if (j < d_cell_particles.extent(1))
+                               d_cell_particles(cell, j) = i;
+                             else
+                               d_resize() = 1;
+                           });
+      Kokkos::Profiling::popRegion();
+      deep_copy(h_resize, d_resize);
+      if(h_resize())
+      {
+	max_particle_per_cell *= 2;
+	d_cell_particles = Kokkos::View<int **>("cell_particles", cell_count, max_particle_per_cell);
+      }
     }
+
+    h_resize() = 1;
+    while(h_resize())
+    {
+      h_resize() = 0;
+      Kokkos::deep_copy(d_resize, h_resize);
+      Kokkos::deep_copy(d_neighbor_count, 0);
+      Kokkos::Profiling::pushRegion("neighbor list");
+      Kokkos::parallel_for("neighbor list", particle_count
+			   , KOKKOS_LAMBDA(const int i)
+                           {
+                             double x = d_x(i);
+                             double y = d_y(i);
+                             double z = d_z(i);
+                             int cell = d_cell_indices(i);
+                             int count = 0;
+                             for (int s = 0; s < stencil_count; s++)
+                             {
+                               int c = cell + d_stencil(s);
+                               if (c >= 0 && c < max_cell_count)
+                               {
+                                 for (int p = 0; p < d_particle_per_cell(c); p++)
+                                 {
+                                   int j = d_cell_particles(c, p);
+                                   double dx = d_x(j) - x;
+                                   double dy = d_y(j) - y;
+                                   double dz = d_z(j) - z;
+                                   double d = sqrt(dx * dx + dy * dy + dz * dz);
+                                   if (d < neighbor_cutoff && i != j)
+                                   {
+                                     if (count < max_neighbor_count - 1)
+                                       d_neighbors(i, count++) = j;
+                                     else
+                                       d_resize() = 1;
+                                   }
+                                 }
+                               }
+                             }
+                             d_neighbor_count(i) = count;
+                           });
+      Kokkos::Profiling::popRegion();
+      Kokkos::deep_copy(h_resize, d_resize);
+      if (h_resize())
+      {
+	max_neighbor_count *= 2;
+	d_neighbors = Kokkos::View<int **>("neighbors", particle_count, max_neighbor_count);
+      }
+    }
+
+    Kokkos::Profiling::pushRegion("pairwise interaction");
+    Kokkos::parallel_for("pairwise interaction", particle_count
+			 , KOKKOS_LAMBDA(const int i)
+                         {
+                           double result0 = 0;
+                           double result1 = 0;
+                           double result2 = 0;
+                           double x = d_x(i);
+                           double y = d_y(i);
+                           double z = d_z(i);
+                           for (int n = 0; n < d_neighbor_count(i); n++)
+                           {
+                             int j = d_neighbors(i, n);
+                             double dx = d_x(n) - x;
+                             double dy = d_y(n) - y;
+                             double dz = d_z(n) - z;
+                             double d = sqrt(dx * dx + dy * dy + dz * dz);
+                             double var0 = d_input0(j) - d_input0(i);
+                             double var1 = d_input1(j) - d_input1(i);
+                             double var2 = d_input2(j) - d_input2(i);
+                             result0 += var0 / d;
+                             result1 += var1 / d;
+                             result2 += var2 / d;
+                           }
+                           d_output0(i) = result0;
+                           d_output1(i) = result1;
+                           d_output2(i) = result2;
+                         });
+    Kokkos::Profiling::popRegion();
   }
 
-  Kokkos::Profiling::pushRegion("pairwise interaction");
-  Kokkos::parallel_for(particle_count, KOKKOS_LAMBDA(const int i)
-    {
-      double result0 = 0;
-      double result1 = 0;
-      double result2 = 0;
-      double x = d_x(i);
-      double y = d_y(i);
-      double z = d_z(i);
-      for (int n = 0; n < d_neighbor_count(i); n++)
-      {
-	int j = d_neighbors(i, n);
-	double dx = d_x(n) - x;
-	double dy = d_y(n) - y;
-	double dz = d_z(n) - z;
-	double d = sqrt(dx * dx + dy * dy + dz * dz);
-	double var0 = d_input0(j) - d_input0(i);
-	double var1 = d_input1(j) - d_input1(i);
-	double var2 = d_input2(j) - d_input2(i);
-	result0 += var0 / d;
-	result1 += var1 / d;
-	result2 += var2 / d;
-      }
-      d_output0(i) = result0;
-      d_output1(i) = result1;
-      d_output2(i) = result2;
-    });
-  Kokkos::Profiling::popRegion();
+  double neighbor_count_sum = 0;
+  Kokkos::parallel_reduce(particle_count
+                          , KOKKOS_LAMBDA(const int i, double &update)
+                          {
+                            update += d_neighbor_count(i);
+                          }, neighbor_count_sum);
+  double neighbor_count_average = neighbor_count_sum / particle_count;
+  
+  double neighbor_count_square_sum = 0;                            
+  Kokkos::parallel_reduce(particle_count
+                          , KOKKOS_LAMBDA(const int i, double &update)
+                          {
+                            int count = d_neighbor_count(i);
+                            update += count * count;
+                          }, neighbor_count_square_sum);
+  double neighbor_count_variance = neighbor_count_square_sum / particle_count;
+
+  std::cout << "# of neighbors: (" << neighbor_count_average << ", " << std::sqrt(neighbor_count_variance) << ")" << std::endl;
 
   /*
   Kokkos::deep_copy(h_x, d_x);
   Kokkos::deep_copy(h_y, d_y);
   Kokkos::deep_copy(h_z, d_z);
 
+  std::cout << "stencil: ";
+  for (int i = 0; i < stencil_count; i++)
+  {
+    std::cout << "(" << i << ": " << h_stencil(i) << ") ";
+  }
+  std::cout << std::endl;
+  
   auto h_cell_indices = Kokkos::create_mirror_view(d_cell_indices);
   Kokkos::deep_copy(h_cell_indices, d_cell_indices);
   auto h_particle_per_cell = Kokkos::create_mirror_view(d_particle_per_cell);
@@ -363,5 +386,6 @@ int main(int argc, char **argv)
     std::cout << std::endl;
   }
   */
+  
   return 0;
 }
